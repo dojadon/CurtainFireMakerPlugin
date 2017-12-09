bc5991facf3e20a4c1bd2581026acf86789d8b5b
zyando <zyando.iron79@gmail.com>
2017-12-09 16:13:34 +0900

    [Move] CubicBezierCurveとMotionIterpolationをEntityShotに移動

--- CurtainFireMakerPlugin/Entities/EntityShot.cs
+++ CurtainFireMakerPlugin/Entities/EntityShot.cs
@@ -1,7 +1,9 @@
-﻿using CsMmdDataIO.Pmx;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CsMmdDataIO.Pmx;
 using CsMmdDataIO.Vmd;
 using CurtainFireMakerPlugin.ShotTypes;
-using System;
 using VecMath;
 
 namespace CurtainFireMakerPlugin.Entities
@@ -219,6 +221,108 @@ namespace CurtainFireMakerPlugin.Entities
                 ModelData.AddMorph(morph);
             }
             return ModelData.MorphDict[morphName];
+        }
+    }
+
+    public struct CubicBezierCurve
+    {
+        public static readonly CubicBezierCurve Line = new CubicBezierCurve(new Vector2(0, 0), new Vector2(0.5F, 0.5F), new Vector2(0.5F, 0.5F), new Vector2(1, 1));
+
+        public Vector2 P0 { get; }
+        public Vector2 P1 { get; }
+        public Vector2 P2 { get; }
+        public Vector2 P3 { get; }
+
+        public CubicBezierCurve(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+        {
+            P0 = p0;
+            P1 = p1;
+            P2 = p2;
+            P3 = p3;
+        }
+
+        public float[] SolveTimeFromX(float x, float eps = 1.0E-4F)
+        {
+            float a0 = P0.x - x;
+            float a1 = 3 * (-P0.x + P1.x);
+            float a2 = 3 * (P0.x - 2 * P1.x + P2.x);
+            float a3 = -P0.x + P3.x + 3 * (P1.x - P2.x);
+
+            double[] solution = EquationUtil.SolveCubic(a3, a2, a1, a0);
+
+            return (from d in solution where (0 - eps <= d && d <= 1 + eps) select (float)d).Distinct().ToArray();
+        }
+
+        public float X(float t) => GetPosition(t, P0.x, P1.x, P2.x, P3.x);
+
+        public float Y(float t) => GetPosition(t, P0.y, P1.y, P2.y, P3.y);
+
+        public float GetPosition(float t, float p0, float p1, float p2, float p3)
+        {
+            float inv = 1 - t;
+            return inv * inv * inv * p0 + 3 * inv * inv * t * p1 + 3 * inv * t * t * p2 + t * t * t * p3;
+        }
+    }
+
+    internal class MotionInterpolation
+    {
+        public CubicBezierCurve Curve { get; }
+        public int StartFrame { get; }
+        public int EndFrame { get; }
+        public int Length { get; }
+        public bool IsSync { get; }
+
+        public MotionInterpolation(int startFrame, int length, Vector2 p1, Vector2 p2, bool isSync)
+        {
+            Curve = new CubicBezierCurve(new Vector2(0, 0), p1, p2, new Vector2(1, 1));
+
+            StartFrame = startFrame;
+            Length = length;
+            EndFrame = StartFrame + Length;
+            IsSync = isSync;
+        }
+
+        public bool Within(int frame)
+        {
+            return StartFrame <= frame && frame < EndFrame;
+        }
+
+        public float GetChangeAmount(int frame)
+        {
+            if (Within(frame))
+            {
+                float unit = 1.0F / Length;
+
+                float x1 = (frame - StartFrame) * unit;
+                float x2 = x1 + unit;
+
+                return (FuncY(x2) - FuncY(x1)) * Length;
+            }
+            else
+            {
+                return 1.0F;
+            }
+        }
+
+        public float FuncY(float x)
+        {
+            float[] t = Curve.SolveTimeFromX(x);
+
+            if (t.Length == 0)
+            {
+                throw new ArithmeticException($"ベジエ曲線の解が見つかりません : x[ {x} ]");
+            }
+
+            float time = t[0];
+
+            if (t.Length > 1)
+            {
+                for (int i = 1; i < t.Length; i++)
+                {
+                    if (Math.Abs(x - time) > Math.Abs(x - t[i])) time = t[i];
+                }
+            }
+            return Curve.Y(time);
         }
     }
 }
